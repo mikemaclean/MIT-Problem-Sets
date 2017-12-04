df0ad5b84dc460fe16de8983a3e70d35c2ad71e7
Michael MacLean <maclean.mike18@gmail.com>
2017-12-04 10:55:03 -0400

    First git commit

---
+++ PSet0.py
@@ -0,0 +1,6 @@
+import numpy
+x=int(input("Enter a number for x ")) #https://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_3/Who_Goes_There%3F
+y=int(input("Enter a number for y "))
+print("x**y=" , x**y)
+print("log(x)",numpy.log2(x)) #http://scipy.github.io/old-wiki/pages/Numpy_Example_List
+input("Press Enter to exit")
